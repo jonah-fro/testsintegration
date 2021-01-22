e6803e5bf8007a6ec60b492e0bea82fb951ce49b
Geoffrey <geoffrey.beauny@publicissapient.com>
2021-01-22 10:06:50 +0100

    Add files : tests, config, etc

---
+++ README.md
@@ -0,0 +1,5 @@
+Pour lancer les tests :
+
+Revoir le fichier package.json :
+
+> npm test
---
+++ tests/uber.js
@@ -0,0 +1,16 @@
+describe('Ecosia example', function() {
+
+    beforeEach(browser => browser.url('https://www.ecosia.org/'));
+
+    test('Demo test ecosia.org', function (browser) {
+      browser
+        .waitForElementVisible('body')
+        .assert.titleContains('Ecosia')
+        .assert.visible('input[type=search]')
+        .setValue('input[type=search]', 'nightwatch')
+        .assert.visible('button[type=submit]')
+        .click('button[type=submit]')
+        .assert.containsText('.mainline-results', 'Nightwatch.js')
+        .end();
+    });
+});
